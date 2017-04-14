ef9fce00e5a71699cbcf9d3ccf4a141f0a76cb68
Administrator <administrator@AdminisatorsMBP.lan>
2017-04-14 14:40:22 -0400

    Updated readme, updated docblocks, checked margins

--- server/routes/jobs.routes.js
+++ server/routes/jobs.routes.js
@@ -1,12 +1,20 @@
 const jobRouter = require('express').Router();
 const Job = require('../models/Job.model.js');
 
+
 /**
- * Returns the id company, and link object keys by looping thrrough the data
- * @type {Array}
+ * Gets a all the object data  stored in the server.
+ * @param {Object}   req  Request argument
+ * @param {Object}   res  Response argument
+ * @param {Function} next
  */
 jobRouter.get('/', function showJobData(req, res, next) {
     Job.find()
+        /**
+         *  Takes in an object data from allJobs and maps it to job
+         * @param  {Object}
+         * @return {Object}
+         */
         .then(function returnJob(allJobs) {
             res.json(allJobs.map(function(job) {
                 return {
@@ -16,6 +24,12 @@ jobRouter.get('/', function showJobData(req, res, next) {
                 };
             }));
         })
+        /**
+         *  Takes the err logs it. Moves to the error handler middleware.
+         * an error.
+         * @param {Error} err
+         *@return {Function}   goes to the error handler
+         */
         .catch(function handleError(err) {
             let ourError = new Error('Unable to retrieve jobs');
             ourError.status = 500;
@@ -23,13 +37,23 @@ jobRouter.get('/', function showJobData(req, res, next) {
         });
 });
 
+
 /**
- * [err description]
- * @type {Error}
+ * Gets a single object by id
+ * @param {Object}   req  Request argument
+ * @param {Object}   res  Response argument
+ * @param {Function} next
  */
 jobRouter.get('/:id', function retrieveSingleJob(req, res, next) {
     console.log('req params', req.params);
     Job.findById(req.params.id)
+        /**
+         *  Takes in an object and checks the id. If the id doesnt match, it return
+         * an error.
+         * @param {Object}
+         * @return {Promise}   Moves on with next(err) if !data otherwise returns the
+         *                     returns the job data.
+         */
         .then(function sendBackSingleJob(data) {
             if (!data) {
                 let err = new Error('No job with that ID');
@@ -40,6 +64,11 @@ jobRouter.get('/:id', function retrieveSingleJob(req, res, next) {
                 theJobIFound: data
             });
         })
+        /**
+         *  Takes the err logs it. Moves to the error handler middleware.
+         * an error.
+         * @param {Error} err   Goes to the error handler
+         */
         .catch(function handleIssues(err) {
             console.error(err);
             next(err);
@@ -64,36 +93,63 @@ function addAJob(req, res, next) {
     });
     console.log(req.body.createTime);
     theJobCreated.save()
+        /**
+         *  Takes in a data object and responds by putting it into json as a
+         *  promise
+         * @param  {Object}
+         * @return {Promise}   Moves on with next(err) if !data otherwise returns the
+         *                     returns the job data.
+         */
         .then(function returnResponse(data) {
             res.json(data);
         })
+        /**
+         *  Takes the err sets the status to 422.  Moves to the error handler middleware.
+         *  an error.
+         * @param {Error} err   Goes to the error handler
+         */
         .catch(function handleErrors(err) {
-            let ourError = new Error('Incorrect data'); //needs something else
+            let ourError = new Error('Incorrect data');
             err.status = 422;
             next(err);
         });
-
 }
 
 
 /**
- * [ourError description]
- * @type {Error}
+ * Calls the delete middleware and passes the req and res objects. Takes the next
+ * takes the next function.
+ * @param {Object}   req  Request argument
+ * @param {Object}   res  Response argument
+ * @param {Function} next
  */
 jobRouter.delete("/:id", function deleteAJob(req, res, next) {
     console.log('Job to delete', req.params.id);
     Job.deleteOne({
             "_id": req.params.id
         })
+        /**
+         *  Takes in a data object and responds by putting it into json as a
+         *  promise
+         * @param  {Object}
+         * @return {Promise}   Moves on with next(err) if !data otherwise returns the
+         *                     returns the job data.
+         */
         .then(function returnResponse(data) {
             res.json(data);
         })
+        /**
+         *  Takes the err sets the status to 404.  Moves to the error handler middleware.
+         *  an error.
+         * @param {Error} err   Goes to the error handler
+         */
         .catch(function handleErrors(err) {
             let ourError = new Error('Data not found'); //needs something else
             err.status = 404;
             next(err);
         });
 });
+
 
 jobRouter.post('/', addAJob);
 module.exports = jobRouter;
